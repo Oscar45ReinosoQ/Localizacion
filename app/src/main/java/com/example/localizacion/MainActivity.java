68ebec0cb43be201c26133b179b8d38e1c83b40b
Oscar45ReinosoQ <98630891+Oscar45ReinosoQ@users.noreply.github.com>
2022-03-19 19:10:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/localizacion/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.localizacion;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity  {
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+
+
+
+
+}
